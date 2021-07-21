0e324359b2a7f4174ae9649ff14e2be518c01533
lucas <lucas.2007s@gmail.com>
2021-07-21 10:53:22 -0300

    first commit

---
+++ src/main/java/br/com/lucad/Main.java
@@ -0,0 +1,8 @@
+package br.com.lucad;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/main/java/br/com/lucad/http/MyHttpClient.java
@@ -0,0 +1,4 @@
+package br.com.lucad.http;
+
+public class MyHttpClient {
+}
